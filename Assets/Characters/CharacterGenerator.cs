bdb7d7054404b0ff2495900a1f90d6a649124ace
elysekenny <elyse2.kenny@live.uwe.ac.uk>
2026-09-21 17:10:49 +0100

    Initial character creator
    The character manager has a "Generate character" function which will instantiate a character prefab and setup the data that is randomised on creation. It will need tweaking with chances and anything influenced by upgrades the player gets

--- Assets/Characters/CharacterGenerator.cs
+++ Assets/Characters/CharacterGenerator.cs
@@ -9,10 +9,10 @@ public class CharacterGenerator : MonoBehaviour
     // spawn the created characters
 
     // Enumerators
-    public enum Occupation {Florist, Butcher} // these are all the job roles a character can have
-    public enum Preferences {} // these are all the things characters can like/ dislike
-    public enum PositiveTrait {}
-    public enum NegativeTrait {}
+    public enum Occupation {Florist, Butcher, Doctor, Unemployed, Accountant, Plumber, CEO, Firefighter, Chef, Teacher, Writer, Singer} // these are all the job roles a character can have
+    public enum Preferences {Flowers, Videogames, Books, Painting, Pottery, Yoga, Chess} // these are all the things characters can like/ dislike
+    public enum PositiveTrait {Charismatic, Honest, Optimistic, Calm, Obedient, Kind, Friendly, Wise, Efficient, Loyal, Patient}
+    public enum NegativeTrait {Lazy, Insomniac, Nosey, Thanatophobic, Foolish, Liar, Jealous, Rebellious, Selfish, Fussy, Needy}
 
     public GameObject CharacterPrefab;
 
@@ -34,10 +34,72 @@ public class CharacterGenerator : MonoBehaviour
         GameObject NewCharacter          = Instantiate(CharacterPrefab);
         CharacterData NewCharacterData   = NewCharacter.GetComponent<CharacterData>();
 
+        // Randomised Name
         NewCharacterData.FirstName       = NameData.FirstNames[Random.Range(0, NameData.FirstNames.Length)];
         NewCharacterData.LastName        = NameData.LastNames[Random.Range(0, NameData.LastNames.Length)];
 
+        //Occupation
+        int NumOfJobs                    = System.Enum.GetNames(typeof(Occupation)).Length;
+        NewCharacterData._Occupation     = (Occupation)Random.Range(0,NumOfJobs);
 
-        Debug.Log($"Creating new character:{NewCharacterData.FirstName} {NewCharacterData.LastName}");
+        List<int> PreferencesUsed = new List<int>(); // there shouldnt be repeats in the likes and dislikes list AND there shouldnt be a like in the dislike list
+
+        // CHARACTER LIKES
+        int TotalLikes = Random.Range(1,3);
+        for(int i = 0; i < TotalLikes; i++)
+        {
+            bool IsValid = false;
+            while(!IsValid)
+            {
+                // generate an enum id
+                // ensure the enum is not in the preferences used list
+                int TryIndex = Random.Range(0, System.Enum.GetNames(typeof(Preferences)).Length);
+                if(!PreferencesUsed.Contains(TryIndex))
+                {
+                    // this is valid
+                    NewCharacterData.Likes.Add((Preferences) TryIndex);
+
+                    PreferencesUsed.Add(TryIndex);
+                    IsValid = true; 
+                }
+            }
+        }
+
+        // CHARACTER DISLIKES
+        int TotalDislikes = Random.Range(1,3);
+        for(int i = 0; i < TotalLikes; i++)
+        {
+            bool IsValid = false;
+            while(!IsValid)
+            {
+                // generate an enum id
+                // ensure the enum is not in the preferences used list
+                int TryIndex = Random.Range(0, System.Enum.GetNames(typeof(Preferences)).Length);
+                if(!PreferencesUsed.Contains(TryIndex))
+                {
+                    // this is valid
+                    NewCharacterData.Dislikes.Add((Preferences) TryIndex);
+
+                    PreferencesUsed.Add(TryIndex);
+                    IsValid = true; 
+                }
+            }
+        }
+
+        // Virtues
+        int VirtueChance = Random.Range(0, 101);
+        if(VirtueChance <= 25) // flat 20% chance BUT we can change this to a variable or an influence from upgrades
+        {
+            NewCharacterData.Virtues.Add((PositiveTrait) Random.Range(0, System.Enum.GetNames(typeof(PositiveTrait)).Length));
+        }
+
+        //Flaws
+        int FlawChance = Random.Range(0, 101);
+        if(FlawChance <= 25) // flat 20% chance BUT we can change this to a variable or an influence from upgrades
+        {
+            NewCharacterData.Flaws.Add((NegativeTrait) Random.Range(0, System.Enum.GetNames(typeof(NegativeTrait)).Length));
+        }
+
+        Debug.Log($"Creating new character: {NewCharacterData.FirstName} {NewCharacterData.LastName}");
     }
 }
