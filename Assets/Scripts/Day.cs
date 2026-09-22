b1dac311f0d126a2922e6fc5681f4030419bb13a
ogt16 <ogt16@icloud.com>
2026-09-22 17:31:47 +0100

    Day scene sort of working

--- Assets/Scripts/Day.cs
+++ Assets/Scripts/Day.cs
@@ -1,10 +1,66 @@
+
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Day : MonoBehaviour
 {
+    [SerializeField] CharacterGenerator characterGenerator;
+    [SerializeField] List<CircleCollider2D> spawnPoints;
+
+    int droneQuota = 5;
+    List<GameObject> drones;
+
     private void Start()
     {
+        drones = new List<GameObject>();
+
+        while (drones.Count < droneQuota)
+        {
+            SpawnNewDrone();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        List<GameObject> toRemove = new List<GameObject>();
+
+        foreach (GameObject drone in drones)
+        {
+            if (drone.activeSelf == false)
+            {
+                toRemove.Add(drone);
+            }
+        }
+
+        foreach (GameObject drone in toRemove)
+        {
+            drones.Remove(drone);
+        }
+
+        while (drones.Count < droneQuota)
+        {
+            SpawnNewDrone();
+        }
+    }
+
+    void SpawnNewDrone()
+    {
+        GameObject newDrone = characterGenerator.GenerateCharacter();
+        newDrone.transform.position = SelectSpawnPosition();
+        drones.Add(newDrone);
+    }
+
+    Vector2 SelectSpawnPosition()
+    {
+        if (spawnPoints.Count == 0) { return Vector2.zero; }
+
+        CircleCollider2D spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+        Vector2 unitRandom = Random.insideUnitCircle;
+        unitRandom *= spawnPoint.radius * spawnPoint.transform.localScale;
+        unitRandom += spawnPoint.offset;
+
+        return unitRandom + new Vector2(spawnPoint.transform.position.x, spawnPoint.transform.position.y);
         
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,10 +7,13 @@ public class Player : MonoBehaviour
     float walkSpeed = 10;
     Rigidbody2D rb;
 
+    List<GameObject> collidingWithTrigger;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        collidingWithTrigger = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -18,11 +22,51 @@ public class Player : MonoBehaviour
         
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collidingWithTrigger.Contains(collision.gameObject))
+        {
+            collidingWithTrigger.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collidingWithTrigger.Contains(collision.gameObject))
+        {
+            collidingWithTrigger.Remove(collision.gameObject);
+        }
+    }
+
     public void OnMove(InputValue input)
     {
         if (rb == null) { return; }
 
         Vector2 value = input.Get<Vector2>();
         rb.linearVelocity = value * walkSpeed;
+    }
+
+    public void OnAttack()
+    {
+        List<GameObject> toRemove = new List<GameObject>();
+
+        foreach (GameObject obj in collidingWithTrigger)
+        {
+            if (obj != null)
+            {
+                if (obj.CompareTag("Character"))
+                {
+                    // Character is removed from drone list in day scene on next fixed update call
+                    GameData.Instance.AddFollower(obj);
+                    toRemove.Add(obj);
+                }
+            }
+        }
+
+        foreach (GameObject obj in toRemove)
+        {
+            collidingWithTrigger.Remove(obj);
+            obj.SetActive(false);
+        }
     }
 }
