b3d92e357d3d7cb3c34200a4883754a1d0d8601d
ogt16 <ogt16@icloud.com>
2026-09-22 13:40:10 +0100

    Added follower list in GameData

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameData : MonoBehaviour
 {
     public static GameData Instance { get; private set; }
+    public List<GameObject> followers;
 
     private void Awake() 
     { 
@@ -18,6 +20,23 @@ public class GameData : MonoBehaviour
         } 
     }
     
+    public void AddFollower(GameObject follower)
+    {
+        if (follower.CompareTag("Character"))
+        {
+            if (!followers.Contains(follower))
+            {
+                followers.Add(follower);
+            }
+        }
+    }
 
+    public void RemoveFollower(GameObject follower)
+    {
+        if (followers.Contains(follower))
+        {
+            followers.Remove(follower);
+        }
+    }
 
 }
