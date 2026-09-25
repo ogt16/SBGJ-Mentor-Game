b2a93e5de79f8fbf995424c10c577267ff05c214
elysekenny <elyse2.kenny@live.uwe.ac.uk>
2026-09-25 12:29:43 +0100

    Minor ui tweaks and made the camera BG black

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -10,7 +10,6 @@ public class Menu : MonoBehaviour
     [SerializeField] Image unpressedImage;
     [SerializeField] Image hoverImage;
     [SerializeField] Image pressedImage;
-    SceneAsset DayScene;
 
     float defaultScale;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
